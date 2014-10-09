6b74e510ba6ead54fe5e018b1b1d6d8b05c18f41
A. van der Pluijm <info@urbanlink.nl>
2014-10-09 12:07:36 +0200

    removed old addReference link
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- packages/datasets/public/controllers/datasets.js
+++ packages/datasets/public/controllers/datasets.js
@@ -365,11 +365,6 @@ angular.module('mean.datasets')
 
 
     /* --------- ROOTSCOPE EVENTS ------------------------------------------------------------ */
-    $rootScope.$on('embedly::new', function(a,b){
-      console.log('a',a);
-      console.log('b',b);
-      addReference(b);
-    });
 
 
     /* --------- INIT ------------------------------------------------------------------------ */
@@ -379,111 +374,5 @@ angular.module('mean.datasets')
       initiateNewDataset();
       toggleEditMode(true);
     }
-  }
-]);
-
-
-angular.module('mean.datasets').controller('DatasetEditCtrl', ['$scope', '$state', '$stateParams', '$timeout', '$interval', 'DatasetSrv',
-  function($scope, $state, $stateParams, $timeout, $interval, DatasetSrv) {
-
-    /*** SCOPE INITIALIZATION ***/
-    $scope.contentLoading = true;
-
-    /*** VARIABLES ***/
-    var autoSaveTimer;
-
-
-    $scope.isEmpty = function(obj) {
-      return angular.equals({}, obj);
-    };
-
-    /*** FUNCTIONS ***/
-    function autoSave() {
-      console.log('Starting autosave');
-      // check for changes in the model
-      if ($scope.draftWorking.description === '') {
-        delete $scope.draftWorking.description;
-      }
-
-      if (!angular.equals($scope.draftMaster, $scope.draftWorking)) {
-
-        $scope.autosaveInProgress = true;
-
-        var d = {
-          id: $scope.dataset._id,
-          draft: $scope.draftWorking
-        };
-
-        DatasetSrv.autoSave(d).then(function(res){
-
-          console.log('Autosave response', res);
-          // Update the main dataset
-          if (res.draft === null ) {
-            res.draft = {};
-          }
-          $scope.dataset = angular.copy(res);
-          console.log('res', $scope.dataset);
-          // update the master copy with the received values, or the published values
-          resetDrafts();
-
-          $scope.autosaveInProgress = false;
-        });
-      }
-    }
-
-    function startAutoSave(){
-      autoSaveTimer = $interval(autoSave, 5000);
-    }
-    function stopAutoSave(){
-      if (angular.isDefined(autoSaveTimer)) {
-        $interval.cancel(autoSaveTimer);
-        autoSaveTimer = undefined;
-      }
-    }
-    function resetDrafts(){
-      console.log('updating draft models');
-
-      if (!$scope.dataset.draft) $scope.dataset.draft = {};
-
-      $scope.draftMaster = {};
-      $scope.draftWorking = {};
-
-      $scope.draftMaster.title = $scope.dataset.draft.title || $scope.dataset.title || null;
-      $scope.draftMaster.description = $scope.dataset.draft.description || $scope.dataset.description || null;
-      $scope.draftWorking = angular.copy($scope.draftMaster);
-      console.log('draftMaster', $scope.draftMaster);
-      console.log('draftWorking', $scope.draftWorking);
-    }
-
-    // Load dataset
-    function loadDataset(){
-      DatasetSrv.retrieve($stateParams.datasetId).then(function(res){
-        if (res._id) {
-          console.log('Dataset received', res);
-          $scope.dataset = res;
-
-          // inititiate the draft master version (for reference)
-          resetDrafts();
-
-          // start the timer for autosave (notice: no check if server response is ok... )
-          startAutoSave();
-
-          // Open sidebar
-          angular.element('.editorSidebar').addClass('isOpen');
-          angular.element('body').addClass('editorSidebarOpen');
-
-        } else {
-          console.log('error or something', res);
-        }
-
-        $scope.contentLoading = false;
-      });
-    }
-
-    /*** ROOTSCOPE EVENTS ***/
-    $scope.$on('stopAutosave', stopAutoSave());
-
-    /*** INIT ***/
-    loadDataset();
   }
 ]);
