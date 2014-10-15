f04f76175023bfa351724c9aa452a9ebd976e10d
A. van der Pluijm <info@urbanlink.nl>
2014-10-15 09:15:28 +0200

    client env vars
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- config/public/config.js
+++ config/public/config.js
@@ -6,11 +6,8 @@ angular.module('mean.system')
     aws: {
       publicKey : '@@AWS_ACCESS_KEY_ID',
       bucket    : '@@S3_BUCKET_NAME',
-      endpoint  : '@@AWS_S3_ENDPOINT',
-    },
-    elasticsearch : {
-      url : '@@ELASTICSEARCH_URL',
-      log : '@@ELASTICSEARCH_LOG'
+      endpoint  : '@@AWS_S3_ENDPOINT'
     }
+
   })
 ;
