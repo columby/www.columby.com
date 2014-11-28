8c2fccc295725b5644d2510b394f671c8ef76145
Marcel Wouters <marcel@thrivingthings.com>
2014-11-28 16:25:05 +0100

    Added initial sequelize powered search.

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var config = require('../config/environment/index.js'),
-    Dataset = require('../models/index').Dataset
+    Dataset = require('../models/index').Dataset,
+    Sequelize = require('sequelize')
 ;
 
 
@@ -10,6 +11,29 @@ exports.search = function(req, res) {
   if (!req.query.query) {
     handleError(res, 'error, no query provided');
   }
+
+  var query = JSON.parse(req.query.query);
+  var q = query.body.query.wildcard._all;
+
+  var wheres = [];
+  var _q = q.trim().split(" ");
+  for(var idx=0; idx < _q.length; idx++) {
+      wheres.push({title: { ilike: "%"+_q[idx]+"%" }});
+  }
+
+  Dataset.findAll({
+    where: Sequelize.or(wheres)
+  })
+    .on('sql', console.log)         // BUG! Somehow sequelize does an OR
+    .success(function(datasets){
+        return res.json(datasets);
+    })
+    .error(function(err){
+        console.log('Error: ', err);
+        handleError(res, err);
+    })
+
+  //return res.json({status:"ok", query:query});
 };
 
 
