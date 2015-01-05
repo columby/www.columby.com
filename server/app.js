2617a9f858d8017a820c598cd4cc6b48ac511995
A. van der Pluijm <info@urbanlink.nl>
2015-01-05 21:17:20 +0100

    Clean up server
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- server/app.js
+++ server/app.js
@@ -8,50 +8,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express'),
-    Sequelize = require('sequelize'),
     config = require('./config/environment');
-
-
-// Connect to the mongo database
-//mongoose.connect(config.mongo.uri, config.mongo.options);
-///**
-// *
-// * Database settings
-// *
-// **/
-//var sequelize = new Sequelize(config.db.uri, {
-//    dialect: config.db.dialect,
-//    logging: false,
-//    define: {
-//      underscored: true
-//    }
-//  }
-//);
-//
-///**
-// *
-// * Authenticate to the database
-// *
-// **/
-//sequelize
-//  .authenticate()
-//  .complete(function(err) {
-//    if (!!err) {
-//      console.log('Unable to connect to the database:', err)
-//    } else {
-//      console.log('Postgres; Connection has been established successfully.')
-//    }
-//  });
-
-// Populate DB with sample data
-//if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 require('./config/express')(app);
 
-//var models = require('./models');
 var routes = require('./routes')(app);
 
 // Start server
@@ -60,4 +23,4 @@ server.listen(config.port, config.ip, function () {
 });
 
 // Expose app
-exports = module.exports = app;
+module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,24 @@
 'use strict';
 
-var fs = require('fs'),
-    errors = require('../controllers/errors.controller');
 
 module.exports = function(app) {
 
-  //fs.readdirSync(__dirname).forEach(function(file) {
-  //  if ((file === 'index.js') || (file[0] === "."))  return;
-  //  var name = file.substr(0, file.indexOf('.'));
-  //  require('./' + name + '.routes')(app);
-  //});
-
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-    .get(errors[404]);
+    .get(function (req, res) {
+      var viewFilePath = '404';
+      var statusCode = 404;
+      var result = {
+        status: statusCode
+      };
+
+      res.status(result.status);
+      res.render(viewFilePath, function (err) {
+        if (err) { return res.json(result, result.status); }
+
+        res.render(viewFilePath);
+      });
+    });
 
   // All other routes should redirect to the index.html
   app.route('/*')
