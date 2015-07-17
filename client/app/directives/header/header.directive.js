f1ec60ddeb9f31eee4d2e95a47d1473d832988fe
A. van der Pluijm <info@urbanlink.nl>
2015-07-17 13:11:42 +0200

    autohide header
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- client/app/directives/header/header.directive.js
+++ client/app/directives/header/header.directive.js
@@ -2,12 +2,28 @@
 
 angular.module('columbyApp')
 
-  .directive('header', function() {
+  .directive('header', function($document, $window) {
 
     return {
       restrict: 'EA',
       templateUrl: 'app/directives/header/header.html',
-      controller: function($rootScope,$scope,$state,AuthSrv) {
+
+      controller: function($window, $timeout, $rootScope,$scope,$state,AuthSrv) {
+        var scrollOriginal = $window.pageYOffset;
+
+        $scope.hideHeader = false;
+
+        // Hide header on scrollDown > 100px
+        angular.element($window).bind("scroll", function() {
+          var scrollNew = $window.pageYOffset;
+          if ( (scrollNew > 100) && (scrollNew > scrollOriginal) ){
+            $scope.hideHeader = true;
+          } else {
+            $scope.hideHeader=false;
+          }
+          $scope.$apply();
+          scrollOriginal=scrollNew;
+        });
 
         $scope.toggleSidebar = function($event){
           $event.preventDefault();
