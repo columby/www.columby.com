5070c96397eea70e105bdfb0e455a6e385ec2d2e
A. van der Pluijm <info@urbanlink.nl>
2015-10-15 15:25:33 +0200

    style update

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,45 +1,33 @@
-(function() {
-  'use strict';
+(function () {
+  'use strict'
 
-  angular
-    .module('columbyApp')
-    .config(config);
-
-  /** @ngInject */
-  function config($logProvider, $locationProvider, $urlRouterProvider, $authProvider, appConstants) {
+  angular.module('columbyApp').config (function ($logProvider, $locationProvider, $urlRouterProvider, $authProvider, appConstants) {
     // Enable log
-    $logProvider.debugEnabled(appConstants.debug);
+    $logProvider.debugEnabled(appConstants.debug)
 
-
-    /***
-     *
+    /* **
      * Use the HTML5 History API
      * For any unmatched url, redirect to /
-     *
-     ***/
-    $locationProvider.html5Mode(true).hashPrefix('!');
-    $urlRouterProvider.otherwise('/');
+     * **/
+    $locationProvider.html5Mode(true).hashPrefix('!')
+    $urlRouterProvider.otherwise('/')
 
-
-    /***
-     *
+    /* **
      * Authentication configuration.
      *   Satellizer settings
-     *
-     ***/
+     * **/
     // Setup token name
-    $authProvider.tokenName = 'token';
-    $authProvider.tokenPrefix = 'columby';
-    $authProvider.withCredentials = false;
+    $authProvider.tokenName = 'token'
+    $authProvider.tokenPrefix = 'columby'
+    $authProvider.withCredentials = false
     // Setup Google authentication provider
     $authProvider.google({
-      url               : appConstants.apiRoot+'/v2/auth/google',
-      clientId          : '313936519511-lklf0npbiafkj3iregop6cle0n8hrpd9.apps.googleusercontent.com',
-      scope             : ['profile', 'email'],
-      redirectUri       : window.location.origin || window.location.protocol + '//' + window.location.host,
-      requiredUrlParams : ['scope'],
-      optionalUrlParams : ['display']
-    });
-  }
-
-})();
+      url: appConstants.apiRoot + '/v2/auth/google',
+      clientId: '313936519511-lklf0npbiafkj3iregop6cle0n8hrpd9.apps.googleusercontent.com',
+      scope: ['profile', 'email'],
+      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+      requiredUrlParams: ['scope'],
+      optionalUrlParams: ['display']
+    })
+  })
+})()
