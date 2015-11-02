1454f9c7b9388af172c7472c88f9fa88050dc822
A. van der Pluijm <info@urbanlink.nl>
2015-11-02 22:18:11 +0100

    Tag index page pagination. Fixes #120
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- src/app/controllers/tag/tag.controller.js
+++ src/app/controllers/tag/tag.controller.js
@@ -1,18 +1,39 @@
 (function() {
   'use strict';
 
-  angular
-    .module('columbyApp')
-    .controller('TagCtrl', function ($log,$scope, $stateParams, TagService) {
+  angular.module('columbyApp')
+  .controller('TagCtrl', function ($log,$scope, $stateParams, TagService) {
+
+    $scope.pagination = {
+      currentPage: 1,
+      numberOfItems: 10,
+      numPages: 1,
+      offset:0
+    };
 
     function getDatasets(){
-      $log.debug($stateParams);
+      var options = {
+        offset: ($scope.pagination.currentPage - 1) * $scope.pagination.numberOfItems,
+        slug: $stateParams.slug
+      };
 
-      TagService.get({slug: $stateParams.slug}, function(result){
+      TagService.get(options, function(result){
         $log.debug(result);
+        // Set number of pagination pages based on result
+        $scope.pagination.numPages = parseInt(result.hits.datasets.count/$scope.pagination.numberOfItems + 1);
+        // Add result to the scope
         $scope.tag = result;
       });
     }
+    $scope.setPage = function (pageNo) {
+      console.log(pageNo);
+      $scope.pagination.currentPage = pageNo;
+    };
+
+    $scope.pageChanged = function() {
+      console.log($scope.pagination.currentPage);
+      getDatasets();
+    };
 
     getDatasets();
 
